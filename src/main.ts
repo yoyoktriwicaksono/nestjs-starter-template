0eb79a07ffe49c32273513d2b3f0c8d92bbd4805
YoyokTriWicaksono <yoyok.thewicaksono@gmail.com>
2020-12-14 23:33:49 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { environment } from '../environment/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(environment.port);
 }
 bootstrap();
